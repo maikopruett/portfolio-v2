c1565828a1d65049c6b9609fac0d2e324d8d8f01
Maiko Pruett <maikopruett@gmail.com>
2025-01-09 23:26:24 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ import './App.css'
 function App() {
   return (
     <>
-      <ShaderGradientCanvas
-        style={{
-          position: 'absolute',
-          top: 0,
-        }}
-      >
-        <ShaderGradient
-          control='query'
-          urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.2&cAzimuthAngle=180&cDistance=3.6&cPolarAngle=90&cameraZoom=1&color1=%2352ff89&color2=%23dbba95&color3=%23d0bce1&embedMode=off&envPreset=city&fov=45&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=-1.4&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=10&rotationZ=0&shader=defaults&type=plane&uDensity=1.3&uFrequency=5.5&uSpeed=0.4&uStrength=4&uTime=0&wireframe=false&zoomOut=false'
-        />
-      </ShaderGradientCanvas>
+      <div className='app-bg'>
+        <ShaderGradientCanvas
+          style={{
+            position: 'absolute',
+            top: 0,
+          }}
+        >
+          <ShaderGradient
+            control='query'
+            urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=15.1&color1=%2373bfc4&color2=%23ff810a&color3=%238da0ce&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&type=sphere&uAmplitude=3.2&uDensity=0.8&uFrequency=5.5&uSpeed=0.3&uStrength=0.3&uTime=0&wireframe=false'
+          />
+        </ShaderGradientCanvas>
+      </div>
+      <div className='app-info'>
+
+      </div>
     </>
   )
 }
