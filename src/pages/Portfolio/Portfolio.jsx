e095a6caaf9fa84fefdf7f736fcb6b116537ed63
Maiko Pruett <maikopruett@gmail.com>
2025-01-11 18:37:20 -0800

    added dates

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -13,15 +13,16 @@ export default function Portfolio() {
             <div className='portfolio-card'>
                 <div id='portfolio-card-bg1' className='portfolio-card-bg'></div>
                 <p className='portfolio-card-title'>Snappify</p>
-                <p className='portfolio-card-date'>date</p>
+                <p className='portfolio-card-date'>Tue Jan 2 2025</p>
                 <div className='portfolio-card-languages'>
                     <p className='portfolio-card-language'>React Js</p>
+                    <p className='portfolio-card-language'>Calendly</p>
                 </div>
             </div>
             <div className='portfolio-card'>
                 <div id='portfolio-card-bg2' className='portfolio-card-bg'></div>
                 <p className='portfolio-card-title'>Portfolio</p>
-                <p className='portfolio-card-date'>date</p>
+                <p className='portfolio-card-date'>Fri Jan 10 2025</p>
                 <div className='portfolio-card-languages'>
                     <p className='portfolio-card-language'>React Js</p>
                 </div>
@@ -29,9 +30,10 @@ export default function Portfolio() {
             <div className='portfolio-card'>
                 <div id='portfolio-card-bg3' className='portfolio-card-bg'></div>
                 <p className='portfolio-card-title'>OSU Dev Club</p>
-                <p className='portfolio-card-date'>date</p>
+                <p className='portfolio-card-date'>Mon Dec 20 2024</p>
                 <div className='portfolio-card-languages'>
                     <p className='portfolio-card-language'>React Js</p>
+                    <p className='portfolio-card-language'>Email Js</p>
                 </div>
             </div>
         </div>
